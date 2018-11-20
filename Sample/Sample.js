7c32024fef58f101a45752e59543481c0769214d
myeong chul <10jade0070@gmail.com>
2018-11-20 12:16:31 +0900

    테스트 코드 작성

---
+++ Sample/Sample.js
@@ -0,0 +1,25 @@
+// let name = "myeong"
+// let age = 30;
+// console.log(name);
+
+// let name = "myeong";
+// let len = name.length;
+// console.log(len);
+
+// let newName = name.replace("myeong","chul");
+// console.log(newName);
+
+// let char = newName.charAt(2);
+// console.log(char);
+
+// console.log(newName.toUpperCase());
+// console.log(newName.toLowerCase());
+
+var x = 0;
+x += 5;
+console.log(x);
+
+console.log("hello " + "world");
+console.log('1' + 2 + 3 + 4);
+
+console.log(123 == '123');
